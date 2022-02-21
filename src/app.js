d91b824eb8df6a27f181e1dcd0c4e32b3e6d4076
Nikhil Chaurasia <nikhil.chaurasia140298@gmail.com>
2022-02-21 18:31:05 +0530

    remove dummy expense

--- src/app.js
+++ src/app.js
@@ -3,19 +3,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import getVisibleExpenses from './selectors/expenses';
 import './styles/styles.scss';
 
 const store = configureStore();
-
-store.dispatch(addExpense({ description: 'Water bill', amount: 500 }));
-store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'rent', amount: 109500 }));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
 
 const jsx = (
     <React.StrictMode>
