3132230894fed0e5f69b45a5f5cbc47dbeb3de70
Nikhil Chaurasia <nikhil.chaurasia140298@gmail.com>
2022-02-26 19:17:16 +0530

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,57 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-    entry: './src/app.js',  // Relative path
-    output: {
-        // __dirname: returns absolute path to project folder
-        // path.join(): joining absolute path with local path
-        path: path.join(__dirname, 'public'),   // Absolute path
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            // what files do we want to run loader on (only js)
-            test: /\.js$/,
-            // lets us exclude given set of files
-            exclude: /node_modules/
-        }, {
-            // test: /\.scss$/,
-            test: /\.(s?)css$/,
-            include: [
-                path.resolve(__dirname, "./src"),
-                path.resolve(__dirname, "./node_modules")
-            ],
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'eval-cheap-module-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
-    },
-    mode: 'development'
+module.exports = (env) => {
+    const isProduction = env.production; 
+
+    console.log('env', env)
+    return {
+        mode: env.production ? "production" : "development",
+        entry: './src/app.js',  // Relative path
+        output: {
+            // __dirname: returns absolute path to project folder
+            // path.join(): joining absolute path with local path
+            path: path.join(__dirname, 'public'),   // Absolute path
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                // what files do we want to run loader on (only js)
+                test: /\.js$/,
+                // lets us exclude given set of files
+                exclude: /node_modules/
+            }, {
+                // test: /\.scss$/,
+                test: /\.(s?)css$/,
+                include: [
+                    path.resolve(__dirname, "./src"),
+                    path.resolve(__dirname, "./node_modules")
+                ],
+                // use provides us with array of loaders
+                use: [MiniCssExtractPlugin.loader, {
+                    loader: 'css-loader',
+                    // Source map 
+                    options: {
+                        sourceMap: true,
+                    }
+                }, {
+                    loader: "sass-loader",
+                    options: {
+                        sourceMap: true,
+                    }
+                }],
+
+            }]
+        },
+        plugins: [
+            new MiniCssExtractPlugin()
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        },
+        mode: 'development'
+    };
 };
